61629e8ca6c2a8297af36a0025b266e2e4e9e2d2
FaEsinoP <shaturnyyy@mail.ru>
2022-06-10 01:42:01 +0300

    very bad my friend

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-def filter_list(l):
-    return [i for i in l if not isinstance(i, str)]
+def duplicate_count(text):
+    slovar = {}
+    count = 0
+    count_all = 0
+    for i in text:
+        for j in text:
+            if j.lower() == i.lower():
+                count += 1
+        if i.lower() not in slovar:
+            slovar[i.lower()] = count
+        count = 0
+    for i in slovar:
+        if slovar[i] > 1:
+            count_all += 1
+    return count_all
+    pass
