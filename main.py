dc69ab031ff269f91641b674baf6164a8ec5bb6f
FaEsinoP <shaturnyyy@mail.ru>
2022-06-10 11:32:23 +0300

    1

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def printer_error(s):
-    count = 0
-    for i in s:
-        if i > "m":
-            count += 1
-    return str(count) + "/" + str(len(s))
+def are_coprime(n, m):
+    list_n = [i for i in range(1, n + 1) if n % i == 0]
+    list_m = [i for i in range(1, m + 1) if m % i == 0]
+    return len(list(set(list_n) & set(list_m))) == 1
