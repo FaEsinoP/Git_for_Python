055fa43b0496a2761c716574a0eb532aa3c552b1
FaEsinoP <shaturnyyy@mail.ru>
2022-06-10 22:31:39 +0300

    qwe\

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-def solution(s):
-    arr = []
-    if len(s) % 2 == 0:
-        for i in range(0, len(s), 2):
-            arr.append(s[i] + s[i + 1])
-    else:
-        for i in range(0, len(s), 2):
-            if i == len(s) - 1:
-                arr.append(s[i] + "_")
-            else:
-                arr.append(s[i] + s[i + 1])
-
-    return arr
+def number_of_pairs(gloves):
+    arr = {}
+    count = 0
+    for i in range(len(gloves)):
+        for j in range(i, len(gloves)):
+            if gloves[i] == gloves[j]:
+                count += 1
+        if gloves[i] not in arr:
+            arr[gloves[i]] = count
+        count = 0
+    for i in arr:
+        count += arr[i] // 2
+    return (count)
