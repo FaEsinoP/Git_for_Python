87fcf72f39a4a05854322bc0c72ea5ad99391cd8
FaEsinoP <shaturnyyy@mail.ru>
2022-06-12 22:42:34 +0300

    123

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def spin_words(sentence):
-    arr = sentence.split(" ")
-    new = []
-    for word in arr:
-        if len(word) >= 5:
-            new.append(word[::-1])
-        else:
-            new.append(word)
-    string = " ".join(new)
-    return string
+def sort_array(source_array):
+    sch = 0
+    nechet = [i for i in source_array if i % 2 == 1]
+    nechet.sort()
+    for i in range(len(source_array)):
+        if source_array[i] % 2 == 1:
+            source_array[i] = nechet[sch]
+            sch += 1
+    return source_array
