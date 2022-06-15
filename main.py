5cb948c69742fcd16066729b955e0f3c008866eb
FaEsinoP <shaturnyyy@mail.ru>
2022-06-15 23:31:46 +0300

    easy first 5 kata

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-def sort_array(source_array):
-    sch = 0
-    nechet = [i for i in source_array if i % 2 == 1]
-    nechet.sort()
-    for i in range(len(source_array)):
-        if source_array[i] % 2 == 1:
-            source_array[i] = nechet[sch]
-            sch += 1
-    return source_array
+def score(dice: list):
+    total = 0
+    one = dice.count(1)
+    two = dice.count(2)
+    three = dice.count(3)
+    four = dice.count(4)
+    five = dice.count(5)
+    six = dice.count(6)
+
+    total += (one // 3) * 1000 + (two // 3) * 200 + (three // 3) * 300 + (four // 3) * 400 + (five // 3) * 500 + \
+             (six // 3) * 600
+    total += (one % 3) * 100 + (five % 3) * 50
+    return total
