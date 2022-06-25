bdcc73f5c2387275c5d569de008556834e7752a0
FaEsinoP <shaturnyyy@mail.ru>
2022-06-25 16:11:13 +0300

    binary_search definition

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-def score(dice: list):
-    total = 0
-    one = dice.count(1)
-    two = dice.count(2)
-    three = dice.count(3)
-    four = dice.count(4)
-    five = dice.count(5)
-    six = dice.count(6)
+def binary_search(A: list, item):
+    height = len(A)
+    low = 0
+    while height >= low:
+        mid = (height + low) // 2
+        if A[mid] == item:
+            return mid
+        if A[mid] > item:
+            height = mid - 1
+        else:
+            low = mid + 1
+    return None
 
-    total += (one // 3) * 1000 + (two // 3) * 200 + (three // 3) * 300 + (four // 3) * 400 + (five // 3) * 500 + \
-             (six // 3) * 600
-    total += (one % 3) * 100 + (five % 3) * 50
-    return total
+
+A = [10, 20, 30, 40, 54]
+
+print(binary_search(A, 40))
