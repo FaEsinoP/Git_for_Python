1f9bd37a1f839f32ca9bedbec2d9113e4efbd63c
FaEsinoP <shaturnyyy@mail.ru>
2022-07-08 00:56:56 +0300

    some shit

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-class Car:
-    def __init__(self, everage, h, w):
-        self.everage_speed = everage
-        self.height = h
-        self.weight = w
+chislo = input()
+arr = list(chislo)
+numbers = arr[-5:]
+numbers.reverse()
+answer = []
+if len(arr) == 5:
+    arr = []
+else:
+    arr = arr[0]
 
-    def start_engine(self):
-        print("RrrRRRrRRrr!")
-
-    def show_max_speed(self, everage_speed):
-        print(f"Max_speed: {everage_speed * 2} км/c")
-
-
-mustang = Car(1, 1, 1)
-mustang.start_engine()
-mustang.show_max_speed(mustang.everage_speed)
+for i in range(len(arr)):
+    answer.append(arr[i])
+for i in range(len(numbers)):
+    answer.append(numbers[i])
+while answer[0] == str(0):
+    answer.pop(0)
+print("".join(answer))
