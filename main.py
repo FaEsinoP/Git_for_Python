0adc9bef469a2403f0415b71254122f9034f651f
FaEsinoP <shaturnyyy@mail.ru>
2022-07-11 22:05:44 +0300

    chess

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-matrix = []
-sum_1 = 0
-sum_2 = 0
-sum_3 = 0
-sum_4 = 0
+n = 8
+doska = [['.'] * n for _ in range(n)]
+k = list(input())
+if k[0] == "a":
+    k[0] = 0
+if k[0] == "b":
+    k[0] = 1
+if k[0] == "c":
+    k[0] = 2
+if k[0] == "d":
+    k[0] = 3
+if k[0] == "e":
+    k[0] = 4
+if k[0] == "f":
+    k[0] = 5
+if k[0] == "g":
+    k[0] = 6
+if k[0] == "h":
+    k[0] = 7
+doska[n - int(k[1])][int(k[0])] = "N"
+nomer, bukva = n - int(k[1]), int(k[0])
 
-n = int(input())
-
-for i in range(n):
-    matrix.append([int(i) for i in input().split()])
 for i in range(n):
     for j in range(n):
-        if j < i > n - j - 1:
-            sum_3 += matrix[i][j]
-        if j < i < n - j - 1:
-            sum_2 += matrix[i][j]
-        if j > i > n - j - 1:
-            sum_4 += matrix[i][j]
-        if j > i < n - j - 1:
-            sum_1 += matrix[i][j]
-print("Верхняя четверть:", sum_1)
-print("Правая четверть:", sum_4)
-print("Нижняя четверть:", sum_3)
-print("Левая четверть:", sum_2)
+        if i == nomer - 2 and ((j == bukva + 1) or (j == bukva - 1)):
+            doska[i][j] = "*"
+        if i == nomer + 2 and ((j == bukva + 1) or (j == bukva - 1)):
+            doska[i][j] = "*"
+        if i == nomer - 1 and ((j == bukva + 2) or (j == bukva - 2)):
+            doska[i][j] = "*"
+        if i == nomer + 1 and ((j == bukva + 2) or (j == bukva - 2)):
+            doska[i][j] = "*"
+for i in range(n):
+    for j in range(n):
+        print(doska[i][j], end=" ")
+    print()
