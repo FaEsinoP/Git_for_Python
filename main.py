0b85f83a34fe2c531357481f189456915e2a5639
FaEsinoP <shaturnyyy@mail.ru>
2022-07-11 17:17:40 +0300

    123

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-arr = input().split()
-result = [[]]
-for i in range(1, len(arr)+1):
-    for j in range(len(arr)):
-        if j == 0:
-            result.append(list(arr[j:i * 1]))
-        elif len(list(arr[j:i + j])) == i:
-            result.append(list(arr[j:i + j]))
-print(result)
+matrix = []
+sum_1 = 0
+sum_2 = 0
+sum_3 = 0
+sum_4 = 0
+
+n = int(input())
+
+for i in range(n):
+    matrix.append([int(i) for i in input().split()])
+for i in range(n):
+    for j in range(n):
+        if j < i > n - j - 1:
+            sum_3 += matrix[i][j]
+        if j < i < n - j - 1:
+            sum_2 += matrix[i][j]
+        if j > i > n - j - 1:
+            sum_4 += matrix[i][j]
+        if j > i < n - j - 1:
+            sum_1 += matrix[i][j]
+print("Верхняя четверть:", sum_1)
+print("Правая четверть:", sum_4)
+print("Нижняя четверть:", sum_3)
+print("Левая четверть:", sum_2)
