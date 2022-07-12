16b5ba38eb2146f08ec644d9afdd24a9e86bb0a8
FaEsinoP <shaturnyyy@mail.ru>
2022-07-12 23:47:47 +0300

    123

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
-n = 8
-doska = [['.'] * n for _ in range(n)]
-k = list(input())
-if k[0] == "a":
-    k[0] = 0
-if k[0] == "b":
-    k[0] = 1
-if k[0] == "c":
-    k[0] = 2
-if k[0] == "d":
-    k[0] = 3
-if k[0] == "e":
-    k[0] = 4
-if k[0] == "f":
-    k[0] = 5
-if k[0] == "g":
-    k[0] = 6
-if k[0] == "h":
-    k[0] = 7
-doska[n - int(k[1])][int(k[0])] = "N"
-nomer, bukva = n - int(k[1]), int(k[0])
-
+n = int(input())
+matrix = [[0 for i in range(n)] for _ in range(n)]
 for i in range(n):
     for j in range(n):
-        if i == nomer - 2 and ((j == bukva + 1) or (j == bukva - 1)):
-            doska[i][j] = "*"
-        if i == nomer + 2 and ((j == bukva + 1) or (j == bukva - 1)):
-            doska[i][j] = "*"
-        if i == nomer - 1 and ((j == bukva + 2) or (j == bukva - 2)):
-            doska[i][j] = "*"
-        if i == nomer + 1 and ((j == bukva + 2) or (j == bukva - 2)):
-            doska[i][j] = "*"
+        for k in range(1, n):
+            if i == j and j < n - k and i < n - k:
+                matrix[i + k][j] = k
+                matrix[i][j + k] = k
 for i in range(n):
     for j in range(n):
-        print(doska[i][j], end=" ")
+        print(matrix[i][j], end=" ")
     print()
