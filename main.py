f4d9f93b331daca2b2f8717533622dc489a0f2d1
FaEsinoP <shaturnyyy@mail.ru>
2022-07-17 02:27:53 +0300

    123

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-n = int(input())
-arr = []
-for i in range(n):
-    myset = set()
-    for j in range(int(input())):
-        myset.add(input())
-    arr.append(myset)
-for i in arr:
-    arr[0].intersection_update(i)
-for i in sorted(arr[0]):
-    print(i)
+morze = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..',
+         'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+         'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '0': '-----',
+         '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+         '9': '----.'}
+word = input()
+for bukva in word:
+    for key, value in morze.items():
+        if bukva.upper() == key:
+            print(value, end=" ")
