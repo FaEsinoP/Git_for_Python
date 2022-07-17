3206a48303847751488fa6bb1490fb2293de9a79
FaEsinoP <shaturnyyy@mail.ru>
2022-07-18 00:26:15 +0300

    123

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-morze = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..',
-         'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
-         'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '0': '-----',
-         '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
-         '9': '----.'}
-word = input()
-for bukva in word:
-    for key, value in morze.items():
-        if bukva.upper() == key:
-            print(value, end=" ")
+secret = input()
+dict_secret = {}
+for i in secret:
+    dict_secret[i] = dict_secret.get(i, 0) + 1
+
+mydict = {}
+n = int(input())
+for _ in range(n):
+    key, value = input().split(": ")
+    mydict[key] = value
+answer = ""
+for i in range(len(secret)):
+    for key, value in dict_secret.items():
+        for key2, value2 in mydict.items():
+            if int(value2) == value:
+                if secret[i] == key:
+                    answer += key2
+print(answer)
